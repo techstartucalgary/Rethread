dfa3d228b56cfb751a971cdf657b4378a464aab1
Axeloooo <axelshz@gmail.com>
2023-11-13 19:36:02 -0700

    feat: Containerization of the backend, docker and developement environment documentation created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,12 @@
 import express, { Express, NextFunction, Request, Response } from "express";
+import cors from "cors";
 import algorithmRouter from "./routers/algorithm.router.js";
-import dotenv from "dotenv";
 import errorHandler from "./middlewares/error.middleware.js";
-
-dotenv.config();
-
-const PORT: number = Number(process.env.PORT) || 8888;
+import { PORT } from "./config/config.js";
 
 export const app: Express = express();
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -15,11 +14,11 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send("Server is up and running!");
 });
 
-app.get("/health", (req: Request, res: Response, next: NextFunction) => {
+app.get("/api/v1/health", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({ status: "healthy" });
 });
 
-app.use("/algorithm", algorithmRouter);
+app.use("/api/v1/algorithm", algorithmRouter);
 
 app.use(errorHandler);
 
