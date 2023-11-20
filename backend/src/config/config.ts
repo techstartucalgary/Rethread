c9f0789bd6941a429d61dc8fa94be443f3db2690
Axeloooo <axelshz@gmail.com>
2023-11-19 23:22:00 -0700

    feat: Session middleware with redis

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,3 +2,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT: number = Number(process.env.PORT) || 8080;
+export const SESSION_SECRET: string = process.env.SESSION_SECRET || "secret";
+export const REDIS_URL: string = process.env.REDIS_URL || "redis";
