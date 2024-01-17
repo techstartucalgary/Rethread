2d9a1ed6e387054236bf883b1c0359c25c06c00c
ryanjung1998 <ryanjung1998@gmail.com>
2024-01-17 16:46:24 -0700

    Created API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import express, { Express } from "express";
 import cors from "cors";
 import { PORT } from "./config/config.js";
-
 import errorHandler from "./middlewares/error.middleware.js";
 
 import algorithmRouter from "./routes/algorithm.routes.js";
 
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 export const app: Express = express();
 
 app.use(express.json());
@@ -13,6 +14,32 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/algorithm", algorithmRouter);
+
+app.post("/product", async (request, response) => {
+  const {title, size, color, description, gender, category, price, imageUrl} = request.body
+  const newProduct = await prisma.product.create({
+      data: {
+          title, size, color, description, gender, category, price, imageUrl
+      }
+  })
+  response.json(newProduct)
+}) 
+
+app.get("/products", async (request, response) => {
+  //add filtering 
+  const prods = await prisma.product.findMany();
+  
+  response.json(prods);
+});
+
+app.get("/product/:id", async (request, response) => {
+  let prodID = request.params.id;
+  const product = await prisma.product.findUnique({
+    where: {id: prodID}
+  })
+  
+  response.json(product);
+});
 
 app.use(errorHandler);
 
